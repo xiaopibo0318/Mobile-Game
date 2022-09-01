99b213f6dcf4b74fb8977029c5d5df67a0be06c4
h855224 <h855224@gmail.com>
2022-09-01 11:24:41 +0800

    0901 cc 2

--- Assets/Script/Particle/ParticleManager.cs
+++ Assets/Script/Particle/ParticleManager.cs
@@ -7,7 +7,7 @@ public class ParticleManager : MonoBehaviour
     [Header("木屑粒子效果")]
     [SerializeField] private List<ParticleSystem> woodParticle = new List<ParticleSystem>();
     [Header("傳送陣粒子效果")]
-    [SerializeField] private List<ParticleSystem> teleportParticle = new List<ParticleSystem>();
+    [SerializeField] private ParticleSystem teleportParticle ;
 
 
 
