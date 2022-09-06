9a05b6d111b1c55fa3c7e192ff02ed90cc5d9244
h855224 <h855224@gmail.com>
2022-09-07 00:42:22 +0800

    2022 09 07 存檔系統測試(未完成)

--- Assets/Script/Save and Load/ObjecttoSave.cs
+++ Assets/Script/Save and Load/ObjecttoSave.cs
@@ -1,31 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjecttoSave : MonoBehaviour// ,ISaveable
+public class ObjecttoSave : MonoBehaviour, ISaveable
 {
     [Header("場景物件")]
-    [SerializeField] private List<GameObject> gameObjects = new List<GameObject>();
-    
-   /* public object SaveState()
-    {
-        /*return new SaveData()
-        {
+    //[SerializeField] private List<GameObject> gameObjects = new List<GameObject>();
+    public GameObject[] ObjectsToSave;
+    //public int a;
 
-        }
-    }*/
+    private void Start()
+    {
+        //GameObject[] ObjectsToSave = new GameObject[gameObjects.Count];
+    }
+
+    public object SaveState()
+    {
+        return new SaveData()
+        {
+            ObjectsToSave = this.ObjectsToSave
+        };
+    }
 
     public void LoadState(object state)
     {
-       
+        var saveData = (SaveData)state;
+        ObjectsToSave = saveData.ObjectsToSave;
     }
 
+
+
+
+    [Serializable]
     private struct SaveData
     {
-        /*for (int i = 0; i<gameObjects.Count; i++)
-        {
-            public GameObject gameObjects[i];
-        }*/
-        
+        public GameObject[] ObjectsToSave;
+        //private List<GameObject> gameObjects;
+        //public int a;
     }
 }
--- Assets/Script/Save and Load/SaveableEntity.cs
+++ Assets/Script/Save and Load/SaveableEntity.cs
@@ -1,18 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SaveableEntity : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string id;
+    public string Id => id;
+
+    [ContextMenu("Generate Id")]
+    private void GeneratedID()
     {
-        
+        id = Guid.NewGuid().ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    // find all ISavable components on gameobject
+    public object SaveState()
     {
-        
+        var state = new Dictionary<string, object>();
+        foreach(var saveable in GetComponents<ISaveable>())
+        {
+            state[saveable.GetType().ToString()] = saveable.SaveState();
+        }
+        return state;
+    }
+
+    public void LoadState(object state)
+    {
+        var stateDictionary = (Dictionary<string, object>)state;
+        foreach (var saveable in GetComponents<ISaveable>())
+        {
+            string typeName = saveable.GetType().ToString();
+            if(stateDictionary.TryGetValue(typeName,out object savedState))
+            {
+                saveable.LoadState(savedState);
+            }
+        }
     }
 }
