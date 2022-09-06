f7f7e1c63eec6709b58d2754f4df5bf109335347
h855224 <h855224@gmail.com>
2022-09-06 15:50:54 +0800

    2022 09 06存檔系統測試(未完成)

---
+++ Assets/Script/Save and Load/ISaveable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISaveable
+{
+    object SaveState();
+    void LoadState(object state); 
+}
---
+++ Assets/Script/Save and Load/ObjecttoSave.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjecttoSave : MonoBehaviour// ,ISaveable
+{
+    [Header("場景物件")]
+    [SerializeField] private List<GameObject> gameObjects = new List<GameObject>();
+    
+   /* public object SaveState()
+    {
+        /*return new SaveData()
+        {
+
+        }
+    }*/
+
+    public void LoadState(object state)
+    {
+       
+    }
+
+    private struct SaveData
+    {
+        /*for (int i = 0; i<gameObjects.Count; i++)
+        {
+            public GameObject gameObjects[i];
+        }*/
+        
+    }
+}
