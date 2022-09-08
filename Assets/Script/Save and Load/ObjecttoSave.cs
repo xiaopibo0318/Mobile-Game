1f8ddc848d5a9ec8c9206cd4aa2854fcc84c79e2
h855224 <h855224@gmail.com>
2022-09-08 16:09:47 +0800

    0908存檔測試1.0(測試成功)

--- Assets/Script/Save and Load/ObjecttoSave.cs
+++ Assets/Script/Save and Load/ObjecttoSave.cs
@@ -5,28 +5,27 @@ using UnityEngine;
 
 public class ObjecttoSave : MonoBehaviour, ISaveable
 {
-    [Header("場景物件")]
-    //[SerializeField] private List<GameObject> gameObjects = new List<GameObject>();
-    public GameObject[] ObjectsToSave;
-    //public int a;
+
+    public string PlayerName = "Howie";
+
 
     private void Start()
     {
-        //GameObject[] ObjectsToSave = new GameObject[gameObjects.Count];
+
     }
 
     public object SaveState()
     {
         return new SaveData()
         {
-            ObjectsToSave = this.ObjectsToSave
+            PlayerName = this.PlayerName
         };
     }
 
     public void LoadState(object state)
     {
         var saveData = (SaveData)state;
-        ObjectsToSave = saveData.ObjectsToSave;
+        PlayerName = saveData.PlayerName;
     }
 
 
@@ -35,8 +34,6 @@ public class ObjecttoSave : MonoBehaviour, ISaveable
     [Serializable]
     private struct SaveData
     {
-        public GameObject[] ObjectsToSave;
-        //private List<GameObject> gameObjects;
-        //public int a;
+        public string PlayerName;
     }
 }
