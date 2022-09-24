8af03562b05622b02a6d0bd13a14a172e8636b7f
xiaopibo0318 <bobo661130@gmail.com>
2022-09-24 19:11:16 +0800

    20220924-存檔一下

---
+++ Assets/Script/GD/arduino/ElectricSlot.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricSlot
+{
+    private int row;
+    private int col;
+
+    private bool redElectric;
+    private bool blackElectric;
+}
