3a73007e15581f9cddaf37304792bc5cc73e214d
h855224 <h855224@gmail.com>
2022-09-30 15:40:08 +0800

    0930 新增音效素材

--- Assets/Script/Audio/AudioManager.cs
+++ Assets/Script/Audio/AudioManager.cs
@@ -14,6 +14,9 @@ public class AudioManager : MonoBehaviour
     [Header("BGM")]
     private AudioSource nowBGM;
 
+    [Header("環境音效")]
+    private AudioSource environmentMusic;
+
 
     public static AudioManager Instance;
 
@@ -36,7 +39,6 @@ public class AudioManager : MonoBehaviour
     private void InitAuudio()
     {
         audioResource = GetComponent<AudioResource>();
-
         nowBGM = gameObject.AddComponent<AudioSource>();
 
     }
@@ -64,9 +66,34 @@ public class AudioManager : MonoBehaviour
         walkOnGrass.volume = 0.1f;
     }
 
-    public void ChangeBgmVolume( float volumeSize)
+    public void ChangeBgmVolume(float volumeSize)
     {
         nowBGM.volume = volumeSize;
     }
 
+
+    private void InitEnvironmentMusic() //初始化某音效，此處是環境音效
+    {
+        environmentMusic = gameObject.AddComponent<AudioSource>();  //先成立一個AudioSource
+        environmentMusic.clip = audioResource.NightForest;  //這個AudioSource要用的檔案在這(從已經掛在AudioResource的音樂裡面去選)
+        environmentMusic.loop = true; //是否重複播放
+        environmentMusic.volume = 0.1f;  //聲音初始值都設為0.1f
+
+       // ChangeEnviormentMusic(audioResource.Forest); //若要變更某環境音效(或其他音效)，則在該腳本呼叫改變的函數
+    }
+    //已環境音效為例，若今天進入不同階段則要換背景音樂，那就在切換階段的那個腳本裡呼叫下面的ChangeEnviormentMusic函數
+
+    private void ChangeEnviormentMusic(AudioClip nowAudioClip)
+    {
+        environmentMusic.clip = nowAudioClip;
+    }
+    //ChangeEnviormentMusic(audioResource.Forest);
+
+
+    //若不需要這個音效了可以把它關掉
+    private void DestroyMusic(AudioSource audiosource)
+    {
+        Destroy(audioResource);
+    }
+    
 }
--- Assets/Script/Audio/AudioResource.cs
+++ Assets/Script/Audio/AudioResource.cs
@@ -7,10 +7,35 @@ public class AudioResource : MonoBehaviour
     [Header("BGM"),SerializeField]
     public AudioClip bgmMTKL;
 
-    [Header("音效"), SerializeField]
+    [Header("環境音效"), SerializeField]
     public AudioClip rain;
+    public AudioClip WaterDrop;
+    public AudioClip NightForest;
+    public AudioClip Forest;
+    public AudioClip ChangeState;
+
+    [Header("遊戲音效"), SerializeField]
+    public AudioClip Salvage;
+    public AudioClip Countdown1;
+    public AudioClip Countdown2;
+    public AudioClip OpenDoor;
+    public AudioClip LotusButton;
+
+    [Header("玩家音效"), SerializeField]
     public AudioClip walkOnGrass;
+    public AudioClip WalkIndoor;
+    public AudioClip WalkOnWater;
 
-
+    [Header("UI音效"), SerializeField]
+    public AudioClip GeneralButton;
+    public AudioClip ConfirmButton;
+    public AudioClip PopUp;
+    
+    
+   
+    
+    
+   
+    
 
 }
