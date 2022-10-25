4d8e842091b16ea36500e2075415e9699382ecef
xiaopibo0318 <bobo661130@gmail.com>
2022-10-25 15:57:26 +0800

    20221025-AddLog

--- Assets/Script/NameSpace/TouchEvent.cs
+++ Assets/Script/NameSpace/TouchEvent.cs
@@ -16,21 +16,25 @@ namespace TouchEvent_Handler
         private static Vector2 endPos = Vector2.zero;
         private static Vector2 moveDirection = Vector2.zero;
 
+        
 
         public void TouchDetect()
         {
-            if (Input.touchCount == 1)
+            Debug.Log("有典籍到物品A");
+            Debug.Log("典籍數量：" + Input.touchCount);
+            if (Input.touchCount > 0)
             {
                 Touch touch = Input.GetTouch(0);
                 //bool isTouchUI = EventSystem.current.IsPointerOverGameObject(touch.fingerId);
                 //if (!isTouchUI)
                 //{
-                Debug.Log("有典籍到物品");
+                Debug.Log("有典籍到物品B");
                 switch (touch.phase)
                 {
                     case TouchPhase.Moved:
                         moveDirection = touch.position - startPos;
                         intervals = Time.realtimeSinceStartup - beginTime;
+
                         Hold();
                         break;
                     case TouchPhase.Ended://手離開螢幕時的狀態
